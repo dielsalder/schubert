91cd3a0930e6f449d5f9351c7cb3301332c7d332
Jude He <>
2025-06-13 21:57:05 -0400

    add dagre

--- src/features/flowchart/Chart.tsx
+++ src/features/flowchart/Chart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import {
   ReactFlow,
   Background,
@@ -8,21 +8,71 @@ import {
   addEdge,
   useNodesState,
   useEdgesState,
+  type Edge,
+  ReactFlowProvider,
+  Panel,
+  useReactFlow,
   type OnConnect,
+  type Node,
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 
 import { initialNodes, nodeTypes } from "~/features/flowchart/nodes";
 import { initialEdges, edgeTypes } from "~/features/flowchart/edges";
+import Dagre from "@dagrejs/dagre";
+
+import "@xyflow/react/dist/style.css";
+import type { AppNode } from "./nodes/types";
+
+const rankdir = "TB";
+const getLayoutedElements = (nodes: Node[], edges: Edge[]) => {
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+  g.setGraph({ rankdir });
+
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
+
+  Dagre.layout(g);
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = g.node(node.id);
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
 
 export default function Chart() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((edges) => addEdge(connection, edges)),
     [setEdges],
   );
+
+  const { fitView } = useReactFlow();
+
+  useEffect(() => {
+    const layouted = getLayoutedElements(nodes, edges);
+
+    setNodes([...layouted.nodes] as AppNode[]);
+    setEdges([...layouted.edges]);
+
+    void fitView();
+  }, [JSON.stringify(nodes), JSON.stringify(edges)]);
 
   return (
     <div className="h-screen w-screen">
